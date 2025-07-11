e533ecf4597be4f7b9621b8821f1f9d36d6309e1
Kondol <kyk9702@gmail.com>
2025-07-11 11:06:16 +0900

    Initial Commit

---
+++ Week3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Week3
+//
+//  Created by yk on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(1)
+        print(2)
+        print(3)
+    }
+
+
+}
+
